fbf88b3ef0389e9aa75187aacfefec501142b20e
arpit <tipra2614@gmail.com>
2018-10-20 15:02:39 +0530

    Added new serializers and views

--- Web/UserDetails/serializers.py
+++ Web/UserDetails/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Person, Disease, Medicine, Prognosis
-
-
+from .models import Person, Disease, Medicine, Prognosis, Prescription
 
 
 class UserDetailsSerializer(serializers.ModelSerializer): 
@@ -35,13 +33,23 @@ class MedicineSerializer(serializers.ModelSerializer):
         model = Medicine
         fields = '__all__'
 
+class PrognosisDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prognosis
+        fields = '__all__'
+
+
 class PrognosisDiseaseSerializer(serializers.ModelSerializer):
     diseaseName = serializers.CharField(max_length = 300)
     class Meta:
         model = Prognosis
-        exclude = ['disease', 'medicines', 'person', 'isActive', 'endTime']
+        exclude = ['disease', 'prescriptions', 'person', 'isActive', 'endTime']
 
-class PrognosisMedicineSerializer(serializers.ModelSerializer):
+class PrognosisPrescriptionSerializer(serializers.ModelSerializer):
+    prognosisID = serializers.IntegerField()
+
     medicineName = serializers.CharField(max_length = 300)
     class Meta:
+        model = Prescription
+        exclude = ['medicine', 'estimateEndTime']
         
