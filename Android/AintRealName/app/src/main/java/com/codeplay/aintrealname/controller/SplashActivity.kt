d9a036ac531b39da7d63c9abb78e76587040116e
rahul <jharahul1998@gmail.com>
2018-10-20 10:11:56 +0530

    Make Login and SignUp page in app

---
+++ Android/AintRealName/app/src/main/java/com/codeplay/aintrealname/controller/SplashActivity.kt
@@ -0,0 +1,46 @@
+package com.codeplay.aintrealname.controller
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.codeplay.aintrealname.R
+import com.codeplay.aintrealname.utilities.AppDB
+import com.codeplay.aintrealname.utilities.Constants
+import com.codeplay.aintrealname.utilities.MyPreferences
+import com.codeplay.aintrealname.utilities.MyPreferences.get
+import com.codeplay.aintrealname.utilities.MyPreferences.set
+import com.codeplay.aintrealname.utilities.UserDetails
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var prefs: SharedPreferences
+    lateinit var userToken: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        prefs = MyPreferences.customPrefs(this, Constants.MY_SHARED_PREFERENCE)
+        userToken = prefs[Constants.KEY_TOKEN, Constants.TOKEN_DEFAULT]
+        Log.v("Token", userToken)
+        if(userToken == Constants.TOKEN_DEFAULT){
+            startActivity(Intent(this, SignInActivity::class.java))
+        }else{
+            val user = AppDB.getInstance(this).getUserDetails()
+            UserDetails.name = user.name
+            UserDetails.birthDate = user.birthDate
+            UserDetails.bloodGroup = user.bloodGroup
+            UserDetails.gender = user.gender
+            UserDetails.height = user.height
+            UserDetails.weight = user.weight
+            UserDetails.phoneNo = user.phoneNo
+            UserDetails.pincode = user.pincode
+
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+    }
+
+
+}
---
+++ Android/AintRealName/app/src/main/java/com/codeplay/aintrealname/utilities/Constants.kt
@@ -0,0 +1,28 @@
+package com.codeplay.aintrealname.utilities
+
+object Constants {
+    var token: String = ""
+
+    const val MY_SHARED_PREFERENCE = "mypreference"
+
+    const val AUTHORIZATION_KEY = "Authorization"
+    const val TOKEN_STRING = "Token "
+    const val TOKEN_DEFAULT = "000000"
+    const val KEY_TOKEN = "token"
+
+    const val NAME_KEY = "name"
+    const val GENDER_KEY = "gender"
+    const val BLOOD_GROUP_KEY = "bloodGroup"
+    const val DATE_OF_BIRTH_KEY = "dob"
+    const val MOBILE_NO_KEY = "phoneNumber"
+    const val HEIGHT_KEY = "height"
+    const val WEIGHT_KEY = "weight"
+    const val PINCODE_KEY = "pincode"
+
+
+    val BASE_URL = "https://7a1cb54e.ngrok.io"
+
+    val SIGNUP_URL = BASE_URL + "/auth/registration/"
+    val LOGIN_URL = BASE_URL + "/auth/login/"
+    val USER_DETAILS_URL = BASE_URL + "/user_details/"
+}
